a7f0092c3bccf5ae0aa410aa9625bd2904c07c1b
jose wellington de frança <wellington52378@gmail.com>
2024-01-27 22:25:17 -0300

    Criado a estrutura do todo-list e alguns components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
+import './App.css';
+import AddTask from "./components/AddTask";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const App = () =>{
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar programação ",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: 'fazer compras',
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "ler livro de fisica para prova",
+      completed: true,
+    }
+  ]); 
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+    <AddTask />
+      <Tasks tasks={tasks} />
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Task from "./task";
+
+const Tasks = ({ tasks }) => {
+    return (
+        <>
+        {tasks.map((task) => (
+            <Task task={task} />
+            ))
+        }
+        </>
+    );
+};
+
+export default Tasks;
